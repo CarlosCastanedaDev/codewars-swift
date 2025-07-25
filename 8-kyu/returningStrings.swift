10338c83c66fe77853c0907f6a1d671639fb202b
CarlosCastanedaDev <carlos@castaneda.dev>
2025-07-24 20:19:22 -0500

    solution 7/24/25

---
+++ 8-kyu/returningStrings.swift
@@ -0,0 +1,5 @@
+// Create a function that accepts a parameter representing a name and returns the message: "Hello, <name> how are you doing today?".
+
+func greet(_ name: String) -> String {
+  return "Hello, \(name) how are you doing today?"
+}
