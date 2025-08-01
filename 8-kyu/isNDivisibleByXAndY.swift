4a8d277c5247e7896ac37feef22e1ed8c89528b5
CarlosCastanedaDev <carlos@castaneda.dev>
2025-08-01 11:45:13 -0500

    solution 8/1/25

---
+++ 8-kyu/isNDivisibleByXAndY.swift
@@ -0,0 +1,5 @@
+// Create a function that checks if a number n is divisible by two numbers x AND y. All inputs are positive, non-zero numbers.
+
+func isDivisible(_ n: Int, _ x: Int, _ y: Int) -> Bool {
+    return n % x == 0 && n % y == 0
+}
